2b127f14863bd89d114353c8321f5e3ad2c2f336
elias <eliasbayonajr@gmail.com>
2019-10-20 00:58:48 +0200

    Added basic CNN
    Basic implimentation of a CNN using pytorch.
    1 converlutional layer:
    -> input depth 3
    -> output depth 16
    -> kernel size 3
    -> padding 1
    
    1 maxpool layer

---
+++ ML Cookbook/CNNs/BasicCNN.py
@@ -0,0 +1,16 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+# CNN architecture definition
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # convolutional layer
+        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
+        # max pooling layer
+        self.pool = nn.MaxPool2d(2, 2)
+
+    def forward(self, x):
+        # add sequence of convolutional and max pooling layers
+        x = self.pool(F.relu(self.conv1(x)))
+        return x
