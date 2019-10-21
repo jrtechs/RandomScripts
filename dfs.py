69834fbfb89a79ea2b5e6fa492f2d1f9cf726c6d
NANCY ANAND <41587186+nancyanand2807@users.noreply.github.com>
2019-10-21 20:51:43 +0530

    Update dfs.py

--- dfs.py
+++ dfs.py
@@ -12,7 +12,7 @@ class Graph:
         # default dictionary to store graph 
         self.graph = defaultdict(list) 
   
-    # function to add an edge to graph 
+    
     def addEdge(self, u, v): 
         self.graph[u].append(v) 
   
